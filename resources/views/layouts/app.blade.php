80980b5b8d6cb704fd8806c943fbece33f109a56
rahathossain99 <rhossainrahat@gmail.com>
2024-05-13 10:20:26 +0600

    title image added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
     <link rel="shortcut icon" href="{{asset('favicon_io/favicon.ico')}}">
     <link rel="shortcut icon" sizes="16x16" href="{{asset('favicon_io/favicon-16x16.png')}}">
     <link rel="shortcut icon" sizes="32x32" href="{{asset('favicon_io/favicon-32x32.png')}}">
-    <link rel="apple-touch-icon" href="{{asset('favicon_io/apple-touch-icon.png')}}">
-    <link rel="icon" href="{{asset('favicon_io/android-chrome-192x192.png')}}" sizes="192x192">
-    <link rel="icon" href="{{asset('favicon_io/android-chrome-512x512.png')}}" sizes="512x512">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('sikhi/logo.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('sikhi/logo.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('sikhi/logo.png') }}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
